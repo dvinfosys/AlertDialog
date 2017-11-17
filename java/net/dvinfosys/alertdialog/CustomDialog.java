3609d395c466682bdcd84769c872a69ebc3f2487
DV Infosys <dvinfosys0@gmail.com>
2017-11-17 21:25:52 +0530

    Update and rename .gitignore to java/net/dvinfosys/alertdialog/CustomDialog.java

---
+++ java/net/dvinfosys/alertdialog/CustomDialog.java
@@ -0,0 +1,42 @@
+package net.dvinfosys.alertdialog;
+
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
+
+/**
+ * Created by vaksys-49 on 10/6/17.
+ */
+
+public class CustomDialog extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder=new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater=getActivity().getLayoutInflater();
+        View view=inflater.inflate(R.layout.fragment_main,null);
+        builder.setView(view);
+
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getActivity(), "Nagativi Button was Clicked", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getActivity(), "Position Button was Clicked", Toast.LENGTH_SHORT).show();
+            }
+
+
+        }) ;
+
+        Dialog dialog=builder.create();
+        return dialog;
+    }
+}
